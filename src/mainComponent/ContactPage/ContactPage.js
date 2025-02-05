983f5c0923d9ebcb9351eee4ac4c1fc84dde3bac
Eunice Abieyuwa Igbinedion <eunice.igbinedion@yahoo.com>
2025-02-05 20:55:38 +0100

    Latest Update

--- src/mainComponent/ContactPage/ContactPage.js
+++ src/mainComponent/ContactPage/ContactPage.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import Footer from "../../components/Footer/Footer.js";
-import Navbar from "../../components/Navbar/Navbar.js";
 import ContactUs from '../../components-sub/ContactUs/ContactUs.js';
+import Navbar from '../../components/Navbar/Navbar.js';
 
 const ContactPage = () => {
   return (
