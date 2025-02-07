a09d674d34ad8ddcee0827272fd5c21659d4ad4a
Eunice Abieyuwa Igbinedion <eunice.igbinedion@yahoo.com>
2025-02-07 22:14:32 +0100

    Update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { NavLink, useNavigate } from "react-router-dom";
 import "./Navbar.scss";
@@ -28,7 +28,9 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="logo"><img src={`${process.env.PUBLIC_URL}/GROW Logo.svg`} alt="Logo" /></div>
+      <div className="logo">
+        <img src={`${process.env.PUBLIC_URL}/GROW Logo.svg`} alt="Logo" />
+      </div>
 
       {/* Hamburger Icon */}
       <div className="hamburger-icon" onClick={toggleMenu}>
@@ -37,29 +39,36 @@ const Navbar = () => {
 
       {/* Overlay to close menu when clicked */}
       {isMenuOpen && (
-        <div
-          className="menu-overlay open"
-          onClick={closeMenu}
-        ></div>
+        <div className="menu-overlay open" onClick={closeMenu}></div>
       )}
 
       {/* Navigation Menu */}
       <div className={`menu ${isMenuOpen ? "open" : ""}`}>
         <ul className="nav-links">
           <li>
-            <NavLink to="/" onClick={closeMenu}>Home</NavLink>
+            <NavLink to="/" onClick={closeMenu}>
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/about-us" onClick={closeMenu}>About Us</NavLink>
+            <NavLink to="/about-us" onClick={closeMenu}>
+              About Us
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/portfolio" onClick={closeMenu}>Portfolio</NavLink>
+            <NavLink to="/portfolio" onClick={closeMenu}>
+              Portfolio
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/testimonials" onClick={closeMenu}>Testimonials</NavLink>
+            <NavLink to="/testimonials" onClick={closeMenu}>
+              Testimonials
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/contact" onClick={closeMenu}>Contact</NavLink>
+            <NavLink to="/contact" onClick={closeMenu}>
+              Contact
+            </NavLink>
           </li>
         </ul>
 
